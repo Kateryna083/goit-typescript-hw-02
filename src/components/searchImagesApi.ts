60a3199111de1fe4de8bbf9124d6b6bdb259030c
Kateryna083 <159190252+Kateryna083@users.noreply.github.com>
2024-10-11 10:29:03 +0300

    Update-searchImagesApi

--- src/components/searchImagesApi.ts
+++ src/components/searchImagesApi.ts
@@ -1,9 +1,18 @@
 import axios from "axios";
+import { Image } from "./App/type";
 
 const accessKey = "OtPmhYgrbPxnaCP6C05d-PoZOITMD26CRAkDGjry23o";
 axios.defaults.baseURL = "https://api.unsplash.com/search";
 
-export default async function searchPhotos(topic, page) {
+export interface SearchPhotosResponse {
+  images: Image[];
+  totalPages: number;
+}
+
+export default async function searchPhotos(
+  topic: string,
+  page: number
+): Promise<SearchPhotosResponse> {
   const response = await axios.get("/photos", {
     headers: {
       Authorization: `Client-ID ${accessKey}`,
